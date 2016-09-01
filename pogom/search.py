2b43571f2c420f77eaa1f40847715ffef481fe57
eckstueck <eckstueck@web.de>
2016-09-01 15:36:47 +0200

    fixed webhook for banned accounts

--- pogom/search.py
+++ pogom/search.py
@@ -452,7 +452,7 @@ def search_worker_thread(args, account, search_items_queue, pause_bit, encryptio
                     while now() < end_sleep:
                         status['message'] = 'Worker {} failed more than {} scans; possibly banned account. Sleeping for 2 hour sleep as of {}'.format(account['username'], args.max_failures, long_sleep_started)
                         log.error(status['message'])
-                        whq.put(('banned', format(account['username']))
+                        whq.put(('banned', account['username']))
                         time.sleep(300)
                     break  # exit this loop to have the API recreated
 
